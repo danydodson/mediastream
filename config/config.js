a954127f9d5b6b00d727fcaa7e0842f484e176b7
Dany Dodson <danydodson@gmail.com>
2020-01-29 12:48:33 -0600

    initial commit

---
+++ config/config.js
@@ -0,0 +1,9 @@
+const config = {
+  env: process.env.NODE_ENV || 'development',
+  port: process.env.PORT || 3333,
+  jwtSecret: process.env.JWT_SECRET || "YOUR_secret_key",
+  mongoUri: process.env.MONGODB_URI || process.env.MONGO_HOST || 'mongodb://' + (process.env.IP || 'localhost') + ':' + (process.env.MONGO_PORT || '27017') + '/mediastream-mern',
+  serverUrl: process.env.serverUrl || 'http://localhost:3000'
+}
+
+export default config
